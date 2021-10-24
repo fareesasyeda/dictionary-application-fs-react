270efbb1d0b9f4ad0fbe8a3c0701a7ef8c16c88f
fareesasyeda <82273133+fareesasyeda@users.noreply.github.com>
2021-10-24 12:42:40 -0500

    Added a search keyword function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
 import dictionary from "./dictionary.png";
+import Dictionary from "./Dictionary.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={dictionary}
-          className="App-dictionary-logo img-fluid"
-          alt="dictionary-logo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="App-dictionary-logo img-fluid"
+            alt="dictionary-logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project is{" "}
+            <a
+              href="https://github.com/fareesasyeda/dictionary-application-fs-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://flamboyant-minsky-f071ea.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+            .
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
