e0bf16dad997c1c897d148d28979dcf87d637652
fareesasyeda <82273133+fareesasyeda@users.noreply.github.com>
2021-11-14 19:33:49 -0600

    Added styling for each component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
           />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunrise" />
         </main>
         <footer className="App-footer">
           <small>
